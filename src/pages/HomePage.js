65ce9cc12c48f54e1946eed94b9f976f10ee70b7
iepppop <iepppop@gmail.com>
2024-03-02 17:30:08 +0900

    style: 홈페이지 추가

---
+++ src/pages/HomePage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from '@emotion/styled'
+
+const Wrap = styled.div`
+    width:100%;
+    height:100%;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+`
+function HomePage() {
+  return (
+    <Wrap>
+  gome
+    </Wrap>
+  )
+}
+
+export default HomePage
+
+
