cf369395f5594bae2545f15dc8dd850af3dbe33e
iepppop <iepppop@gmail.com>
2024-03-04 15:38:15 +0900

    feat: dataSlice 추가

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import  userSlice  from "../features/userSlice.js";
+import  dataSlice  from "../features/dataSlice.js";
 
 export const rootReducer = combineReducers({
-    user: userSlice
+    user: userSlice,
+    data: dataSlice
 })
