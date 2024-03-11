78e1ad1c36b0860f0f051c92d4ac42fbd2213587
iepppop <iepppop@gmail.com>
2024-03-11 18:26:26 +0900

    feat: fetchData 수정

--- src/features/dataSlice.js
+++ src/features/dataSlice.js
@@ -1,22 +1,91 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { collection, query, where, onSnapshot, getDocs, doc, addDoc } from "firebase/firestore";
-import { db } from '../firebase.js' ;
+import { collection, query, where, onSnapshot, getDocs, doc, addDoc, serverTimestamp, updateDoc,orderBy, deleteDoc } from "firebase/firestore";
+import { db, auth, storage } from '../firebase.js';
+import { deleteObject, getDownloadURL, ref, uploadString  } from 'firebase/storage';
 
-export const fetchData = createAsyncThunk('user/fetchData', async (cateName, thunkAPI) => {
+const getCategoryNameEng = (category) => {
+    if (category === '드라마') return 'drama';
+    else if (category === '영화') return 'movie';
+    else if (category === '애니메이션') return 'animation';
+    else if (category === '책') return 'book';
+    else return 'weebtoon';
+}
+
+export const fetchData = createAsyncThunk('data/fetchData', async (location, thunkAPI) => {
     try {
-        const querySnapshot = await getDocs(collection(db, "list", '6rIGBZV7WPHNUXGm3KyY', cateName));
+        thunkAPI.dispatch(setPathname(location));
+        let dataList = [];
+        await new Promise((resolve, reject) => {
+            const unsubscribe = onSnapshot(query(collection(db, "list", '6rIGBZV7WPHNUXGm3KyY', location), orderBy("timestamp", "desc")), (querySnapshot) => {
+                dataList = [];
+                querySnapshot.forEach((doc) => {
+                    let item = doc.data();
+                    console.log(item.timestamp)
+                    const unixTimestamp = item.timestamp ? item.timestamp.toDate().getTime() : 0;
+                    dataList.push({ ...item, id: doc.id, timestamp: unixTimestamp });
+                });
+                resolve();
+            }, (error) => {
+                reject(error);
+            });
+            return unsubscribe;
+        });
+        return dataList;
+
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+    }
+});
+
+export const sendData = createAsyncThunk('data/sendData', async ({content,thumbnail,imageList}, thunkAPI) => {
+    try {
         const dataList = [];
-        querySnapshot.forEach((doc) => {
-            dataList.push({ id: doc.id, ...doc.data() }); 
+        const docRef = await addDoc(collection(db, "list", '6rIGBZV7WPHNUXGm3KyY', "inspiration"), {
+            uid: auth.currentUser.uid,
+            displayName: auth.currentUser.displayName,
+            email: auth.currentUser.email,
+            photoURL: auth.currentUser.photoURL,
+            text: content,
+            thumbnail,
+            images: imageList,
+            timestamp: serverTimestamp(),
         });
         return dataList;
     } catch (error) {
         return thunkAPI.rejectWithValue(error.message);
     }
 });
+
+export const DeleteData = createAsyncThunk('data/DeleteData', async ({location, item}, thunkAPI) => {
+    try{
+        const desertRef = ref(storage, `${location}/${auth.currentUser.uid}/${item.images[0].fileName}`);
+        console.log()
+        await deleteObject(desertRef);
+        await deleteDoc(doc(db, "list", "6rIGBZV7WPHNUXGm3KyY",`${location}`,`${item.id}`));
+    }   
+    catch(error){
+        return thunkAPI.rejectWithValue(error.message);
+    }
+})
+
+export const ImageUpload = createAsyncThunk('data/ImageUpload', async ({listName,file}, thunkAPI) => {
+    try {
+        const imageRef = ref(storage, `${listName}/${auth.currentUser.uid}/img`);
+        await uploadString(imageRef, file, "data_url").then(async () => {
+            const downloadURL = await getDownloadURL(imageRef);
+        });
+    }
+    catch (error){
+        return thunkAPI.rejectWithValue(error.message);
+    }
+})
+
 const initialState = {
-    data: null,
+    data: [],
+    category: 'drama',
     isLoading: false,
+    pathname: '',
+    fileList:[],
 }
 
 export const dataSlice = createSlice({
@@ -29,23 +98,45 @@ export const dataSlice = createSlice({
         setLoading: (state, action) => {
             state.isLoading = action.payload;
         },
+        setCategory: (state, action) => {
+            state.category = action.payload;
+        },
+        setPathname: (state, action) => {
+            state.pathname = action.payload;
+        }
     },
     extraReducers: (builder) => {
         builder
-        .addCase(fetchData.pending, (state, action) => {
-            state.data = null;
-        })
-        .addCase(fetchData.fulfilled, (state, action) => {
-            console.log(action.payload)
-            state.data = action.payload;
-            state.isLoading = false;
-        })
-        .addCase(fetchData.rejected, (state, action) => {
-            console.log(action.payload)
-        })
+            .addCase(fetchData.pending, (state, action) => {
+                state.data = null;
+            })
+            .addCase(fetchData.fulfilled, (state, action) => {
+                if (state.pathname === 'review') {
+                    const dataList = action.payload.filter(ele => getCategoryNameEng(ele.category) === state.category);
+                    state.data = dataList;
+                } else {
+                    state.data = action.payload;
+                }
+                console.log('액션페이로드',action.payload)
+                state.isLoading = false;
+            })
+            .addCase(fetchData.rejected, (state, action) => {
+                console.log(action.payload)
+            })
+            .addCase(sendData.fulfilled, (state, action) => {
+                console.log(action.payload)
+                state.isLoading = false;
+            })
+            .addCase(sendData.rejected, (state, action) => {
+                console.log(action.payload)
+            })
+            .addCase(ImageUpload.fulfilled, (state, action) => {
+                state.fileList.appendChild(action.payload);
+                console.log(state.fileList);
+            })
     }
 });
 
-export const { data } = dataSlice.actions;
+export const { data, setCategory, category, setPathname } = dataSlice.actions;
 export default dataSlice.reducer;
 
